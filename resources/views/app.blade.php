43a64febf8bae297f1787e2ba97978910e672eda
Elf Sundae <elf.sundae@gmail.com>
2017-09-22 15:34:41 +0800

    Add zh locale file

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,10 +2,10 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>{{ isset($title) ? $title . ' - ' : null }}Laravel - The PHP Framework For Web Artisans</title>
+    <title>{{ isset($title) ? $title . ' - ' : null }}@lang('Laravel') - @lang('The PHP Framework For Web Artisans')</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="author" content="Taylor Otwell">
-    <meta name="description" content="Laravel - The PHP framework for web artisans.">
+    <meta name="description" content="@lang('Laravel') - The PHP framework for web artisans.">
     <meta name="keywords" content="laravel, php, framework, web, artisans, taylor otwell">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @if (isset($canonical))
