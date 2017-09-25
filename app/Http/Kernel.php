eeed9bbe2d475dd9c6c4f936e15efc18d4bd55fb
Elf Sundae <elf.sundae@gmail.com>
2017-09-25 10:10:39 +0800

    Move ReplaceLinks to the first of middleware group

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
+            \App\Http\Middleware\ReplaceLinks::class,
             \App\Http\Middleware\CacheResponse::class,
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
@@ -31,7 +32,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\ReplaceLinks::class,
         ],
     ];
 }
